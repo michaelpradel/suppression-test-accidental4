5ebae409cd48459e758e33a69758e485b03dad5b
Michael Pradel <michael@binaervarianz.de>
2023-09-20 10:36:13 +0200

    Add new warning at line 10, which get accidentally suppressed by the existing suppression at line 7

--- hello.py
+++ hello.py
@@ -7,3 +7,4 @@ def hello():
     # pylint: disable=missing-format-string-key
     print("\n")
     print("Hello %(first)s %(last)s" % {"First": "John", "last": "Doe"})
+    print("Bye %(first)s %(last)s" % {"first": "John", "Last": "Doe"})
